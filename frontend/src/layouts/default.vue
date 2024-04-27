b5ccddb86a8ceb4f6ab4c6ba3dbbeb9f11d5be09
Maycon Jesus <contato@mayconjesus.dev>
2024-04-27 01:11:35 -0300

    opengraph tags

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -7,3 +7,18 @@
 
     </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+    title: 'João Tattoo',
+    meta: [
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'João Tattoo' },
+        { property: 'og:url', content: 'https://joao-tattoo.vercel.app' },
+        { property: 'og:image', content: 'https://joao-tattoo.vercel.app/logos/logo-branca' },
+        { property: 'og:image:width', content: '2000' },
+        { property: 'og:image:height', content: '2000' },
+        { property: 'og:description', content: 'Tattoo Geek/BlackWork artist' },
+    ]
+})
+</script>
